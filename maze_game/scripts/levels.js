ea0e226646eef18f25a8d33672100ebe8989c407
Kevin McGinley <kevin@bt48.com>
2015-12-11 12:08:09 +0000

    Added new version of the maze game

---
+++ maze_game/scripts/levels.js
@@ -0,0 +1,7 @@
+define(["level1", "level2", "level3", "level4", "level5"], function (Level1, Level2, Level3, Level4, Level5) {
+    var levels = [
+        Level1, Level2, Level3, Level4, Level5
+    ];
+
+    return levels;
+});
