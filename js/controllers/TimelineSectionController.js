d116980a0dfa7502d416b01cafaf5e557a8e024f
Kevin McGinley <kevin@bt48.com>
2015-12-17 17:06:47 +0000

    Set timeline-list to count of list items * 375

--- js/controllers/TimelineSectionController.js
+++ js/controllers/TimelineSectionController.js
@@ -1,4 +1,9 @@
 angular.module('TimelineSectionController', [])
-.controller('TimelineSectionController', function ($scope, $stateParams) {
-	$scope.sectionTimeline = getObjects($scope.timelineItems, "field_section", $stateParams.sectionId);
-});
+	.controller('TimelineSectionController', function ($scope, $stateParams) {
+		$scope.sectionTimeline = getObjects($scope.timelineItems, "field_section", $stateParams.sectionId);
+
+		angular.element(document).ready(function () {
+				document.getElementsByClassName('timeline-list')[0].style.width = ($scope.sectionTimeline.length * 375) + 'px';
+				alert(document.getElementsByClassName('timeline-list')[0].style.width);
+		});
+	});
