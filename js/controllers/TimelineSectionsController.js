133c8976d1fdbdce61fb038048898f49cac0abe3
Feargal O'Kane <feargal@bt48.com>
2016-01-19 16:45:16 +0000

    Updated Jutland App with new styling

--- js/controllers/TimelineSectionsController.js
+++ js/controllers/TimelineSectionsController.js
@@ -2,6 +2,8 @@ angular.module('TimelineSectionsController', [])
 .controller('TimelineSectionsController', function ($scope, $stateParams) {
    
 	$scope.SelectedSectionId = $stateParams.sectionId;  
+	$categoryItem = getObjects($scope.timelineSections, "TID", $stateParams.sectionId);
+	$scope.categoryName = $categoryItem[0]['name']; 
 
 	angular.element(document).ready(function () {
 		//alert(document.getElementById('category-'+$stateParams.sectionId).outerHTML);
