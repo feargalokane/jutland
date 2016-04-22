7c817725e7a1a91ebd5bab034b50b8244b562b59
Kevin McGinley <kevin@bt48.com>
2016-04-22 13:47:20 +0100

    Example of PDF and 360 working as partial templates

---
+++ 360/medal/config.js
@@ -0,0 +1,100 @@
+var productRotationData_1440066361029 = {
+    "custom": {
+        "contexMenu": {"items": [{"action": "URL_OPEN", "title": "", "data": ""}]}
+    },
+    "settings": {
+        "userInterface": {
+            "showArrows": true,
+            "showZoomButtons": true,
+            "defaultCursor": false,
+            "showZoombar": true,
+            "showScreenshotButton": false,
+            "showTogglePanButton": "zoom",
+            "zoomBarClickable": true,
+            "showToggleRotateButton": "zoom",
+            "showToggleFullscreenButton": true,
+            "showResetButton": "zoom",
+            "showTogglePlayButton": true
+        },
+        "rotation": {
+            "rotate": "true",
+            "disabledIfZoom": false,
+            "bounce": false,
+            "rotateDirection": 1,
+            "rotatePeriod": 3,
+            "firstImage": 0,
+            "rotationAxis": "vertical",
+            "autoRestart": 0
+        },
+        "control": {
+            "zoomSteps": 2,
+            "enableSwing": true,
+            "rotateZoom": false,
+            "playAfterDrag": false,
+            "disableClick": false,
+            "disableDrag": false,
+            "arrowInterval": 100,
+            "screenshotFullImage": false,
+            "zoomSetsPan": false,
+            "disableMouseControl": false,
+            "maxZoom": 300,
+            "reverseDrag": false,
+            "mouseWheelZooms": false,
+            "reversePan": false,
+            "dragSpeed": 0.5,
+            "zoomInStopsRotation": true,
+            "zoomOutStartsRotation": true,
+            "clickZoomsIn": false
+        },
+        "preloader": {
+            "preLoadImages": false,
+            "image": 0,
+            "type": "default",
+            "color1": "#303030",
+            "showStartButton": false,
+            "showLoadedImages": false
+        }
+    },
+    "hotspots": null,
+    "images": [
+        {"hotspots": {}, "src": "images/DSC_0407.JPG"},
+        {"hotspots": {}, "src": "images/DSC_0408.JPG"},
+        {"hotspots": {}, "src": "images/DSC_0409.JPG"},
+        {"hotspots": {}, "src": "images/DSC_0410.JPG"},
+        {"hotspots": {}, "src": "images/DSC_0411.JPG"},
+        {"hotspots": {}, "src": "images/DSC_0412.JPG"},
+        {"hotspots": {}, "src": "images/DSC_0413.JPG"},
+        {"hotspots": {}, "src": "images/DSC_0414.JPG"},
+        {"hotspots": {}, "src": "images/DSC_0415.JPG"},
+        {"hotspots": {}, "src": "images/DSC_0416.JPG"},
+        {"hotspots": {}, "src": "images/DSC_0417.JPG"},
+        {"hotspots": {}, "src": "images/DSC_0418.JPG"},
+        {"hotspots": {}, "src": "images/DSC_0419.JPG"},
+        {"hotspots": {}, "src": "images/DSC_0420.JPG"},
+        {"hotspots": {}, "src": "images/DSC_0421.JPG"},
+        {"hotspots": {}, "src": "images/DSC_0422.JPG"},
+        {"hotspots": {}, "src": "images/DSC_0423.JPG"},
+        {"hotspots": {}, "src": "images/DSC_0424.JPG"},
+        {"hotspots": {}, "src": "images/DSC_0425.JPG"},
+        {"hotspots": {}, "src": "images/DSC_0426.JPG"},
+        {"hotspots": {}, "src": "images/DSC_0427.JPG"},
+        {"hotspots": {}, "src": "images/DSC_0428.JPG"},
+        {"hotspots": {}, "src": "images/DSC_0429.JPG"},
+        {"hotspots": {}, "src": "images/DSC_0430.JPG"},
+        {"hotspots": {}, "src": "images/DSC_0431.JPG"},
+        {"hotspots": {}, "src": "images/DSC_0432.JPG"},
+        {"hotspots": {}, "src": "images/DSC_0433.JPG"},
+        {"hotspots": {}, "src": "images/DSC_0434.JPG"},
+        {"hotspots": {}, "src": "images/DSC_0435.JPG"},
+        {"hotspots": {}, "src": "images/DSC_0436.JPG"},
+        {"hotspots": {}, "src": "images/DSC_0437.JPG"},
+        {"hotspots": {}, "src": "images/DSC_0438.JPG"},
+        {"hotspots": {}, "src": "images/DSC_0439.JPG"},
+        {"hotspots": {}, "src": "images/DSC_0440.JPG"}
+    ]
+};
+
+//for maximum compatiblity, assign the data to 1. common named variable, 2. product - specific named variable
+
+var RotationData_test4 = productRotationData_1440066361029;
+var RotationData = productRotationData_1440066361029;
--- js/controllers/TimelineItemController.js
+++ js/controllers/TimelineItemController.js
@@ -1,5 +1,36 @@
-angular.module('TimelineItemController', [])
-.controller('TimelineItemController', function ($scope, $stateParams) {
+angular.module('TimelineItemController', ['ngPDFViewer'])
+.controller('TimelineItemController',['$scope','PDFViewerService','$stateParams', function ($scope,pdf,$stateParams) {
 	timelineItem = getObjects($scope.timelineItems, "nid", $stateParams.id); 
 	$scope.item = timelineItem[0];
-});
+	
+	if(timelineItem[0].type == '360'){
+		$scope.threesixtyurl = "360/"+timelineItem[0].Dir +"/iframe.html"
+	}
+	
+	
+	if(timelineItem[0].type == 'PDF'){
+		$scope.pdfURL = 'pdf/'+timelineItem[0].PDFileName;
+		$scope.instance = pdf.Instance("viewer");
+
+		$scope.nextPage = function() {
+			$scope.instance.nextPage();
+		};
+		
+		$scope.prevPage = function() {
+			$scope.instance.prevPage();
+		};
+	
+		$scope.gotoPage = function(page) {
+			$scope.instance.gotoPage(page);
+		};
+	
+		$scope.pageLoaded = function(curPage, totalPages) {
+			$scope.currentPage = curPage;
+			$scope.totalPages = totalPages;
+		};
+	
+		$scope.loadProgress = function(loaded, total, state) {
+			console.log('loaded =', loaded, 'total =', total, 'state =', state);
+		};		
+	}
+}]);
